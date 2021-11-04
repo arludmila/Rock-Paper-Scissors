431cfbc58405dc542a2ce53a16432131b2df4d1a
arludmila <ludmila062001@gmail.com>
2021-11-03 22:10:03 -0300

    cambios

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const options=["rock","paper","scissors"];
 const user="";
 const pc=options[Math.floor(Math.random()*options.length)];
-userScissors(){
-    user="scissors";
-    alert(user)
+function userScissors(){
+    alert("he")
 }
