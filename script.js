e379befe0f365a1ea9d870b40d524ce7ca2283cb
arludmila <ludmila062001@gmail.com>
2021-11-03 22:44:04 -0300

    antes de juego

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 const options=["rock","paper","scissors"];
-const user="";
-const pc=options[Math.floor(Math.random()*options.length)];
+var user="";
+var pc="";
+
+function userPlay(){
+    return user
+}
+
+function userRock(){
+    user="rock";
+    alert(user);
+    playRound();
+}
+function userPaper(){
+    user="paper";
+    alert(user);
+    userPlay();
+}
 function userScissors(){
-    alert("he")
+    user="scissors";
+    alert(user);
+    userPlay();
+}
+
+function computerPlay(){
+    return options[Math.floor(Math.random()*options.length)];
+}
+
+
+function playRound(){
+    playerSelection=userPlay()
+    computerSelection=computerPlay()
+    console.log("Computer choice: "+computerSelection)
+    if  (playerSelection === "rock" && computerSelection === "paper"){
+         console.log("You lose!");
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+         console.log("You win!");
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+         console.log("You win!");
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+         console.log("You lose!");
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+         console.log("You win!");
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+         console.log("You lose!");
+    }
+    else if(playerSelection===computerSelection){
+         console.log("You tied!")
+    }
 }
